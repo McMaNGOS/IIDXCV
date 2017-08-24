e1b532dbced776aa22971fdf8feace1a627027a4
McMaNGOS <mcmangos1@hotmail.com>
2017-08-24 17:20:32 +0200

    Working on key pressing logic
    Also re-ordered some things

--- IIDXCV.py
+++ IIDXCV.py
@@ -4,34 +4,7 @@ import numpy as np
 import argparse
 import imutils
 import time
-
-# class for constructing key objects
-class Key:
-    'Data for each key (detection pixel x/y location, name)'
-
-    def __init__(self, x, y, name):
-
-        # x and y axis of pixel to check for
-        self.x = x
-        self.y = y
-
-        # name of key (to print in console)
-        self.name = name
-
-        # TODO: add actual key value (for input)
-
-# define keys (8 for IIDX-style games)
-key1 = Key(16, 99, "SCRATCH")
-key2 = Key(70, 99, "KEY 1")
-key3 = Key(104, 99, "KEY 2")
-key4 = Key(135, 99, "KEY 3")
-key5 = Key(169, 99, "KEY 4")
-key6 = Key(199, 99, "KEY 5")
-key7 = Key(232, 99, "KEY 6")
-key8 = Key(263, 99, "KEY 7")
-
-# put keys in array
-keyArray = [key1, key2, key3, key4, key5, key6, key7, key8]
+import pyvjoy
 
 # argument parser (for video, will use stream/live frames in future)
 ap = argparse.ArgumentParser()
@@ -44,8 +17,58 @@ print("Initializing video stream...")
 fvs = FileVideoStream(args["video"]).start()
 time.sleep(1.0)
 
+# class for constructing key objects
+class Key:
+    'Data for each key (detection pixel x/y location, name)'
+
+    def __init__(self, x, y, name, keyButton):
+
+        # x and y axis of pixel to check for
+        self.x = x
+        self.y = y
+
+        # name of key (to print in console)
+        self.name = name
+
+        # vJoy button to press
+        self.keyButton = keyButton
+
+# set key state to on (1)
+def pressKey(key):
+    global keysPressed
+    gamepad.set_button(key.keyButton, 1)
+    keysPressed.append(key)
+
+# set key state to off (0)
+def releaseKey(key):
+    global keysPressed
+    gamepad.set_button(key.keyButton, 0)
+    keysPressed.remove(key)
+
+# LR2 treats the scratch as two normal keys, while IIDX treats it as a
+# rotating axis. Separate function will be needed for IIDX. (pass game as arg?)
+
+# define keys (8 for IIDX-style games)
+scratch = Key(16, 99, "SCRATCH", 1)
+key1 = Key(70, 99, "KEY 1", 2)
+key2 = Key(104, 99, "KEY 2", 3)
+key3 = Key(135, 99, "KEY 3", 4)
+key4 = Key(169, 99, "KEY 4", 5)
+key5 = Key(199, 99, "KEY 5", 6)
+key6 = Key(232, 99, "KEY 6", 7)
+key7 = Key(263, 99, "KEY 7", 8)
+
+# put keys in array
+keyArray = [scratch, key1, key2, key3, key4, key5, key6, key7]
+
+# construct gamepad
+gamepad = pyvjoy.VJoyDevice(1)
+
 # create background subtractor
 bgSub = cv2.createBackgroundSubtractorMOG2()
+
+# array for checking which keys were pressed on a frame
+keysPressed = []
 
 # loop over frames from the video file stream
 while fvs.more():
@@ -64,7 +87,7 @@ while fvs.more():
 
     # keys to print (underscores by default, for readability)
     printArray = ['_______', '_____', '_____', '_____', '_____', '_____', '_____', '_____']
-    initialPrintArray = ['_______', '_____', '_____', '_____', '_____', '_____', '_____', '_____']
+    initialPrintArray = printArray
 
     # loop through keys in array
     for idx, Key in enumerate(keyArray):
@@ -72,11 +95,21 @@ while fvs.more():
         # detect pixel at given coordinates
         pixel = mask[Key.y, Key.x]
 
-        # if white pixel found, add key to printArray
-        if pixel == 255:
+        # if white pixel found, press key, add to keysPressed & print arrays
+        if pixel == 255 && Key not in keysPressed:
+            pressKey(Key)
+            keysPressed.append(Key)
             printArray[idx] = Key.name
 
-    # print if array is different from default (key detected)
+        # if white pixel not found & key is in keysPressed, release & remove from array
+        else if pixel != 255 && Key in keysPressed:
+            releaseKey(Key)
+            keysPressed.remove(Key)
+
+        else
+            pass
+
+    # print if array is different from default (= key detected)
     if printArray != initialPrintArray:
         print printArray
 
